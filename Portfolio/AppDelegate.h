0ed138ccf649fe44f433c9235db9f0052742ab0d
jatinp <jatinp@valleymusic-lm.y.corp.yahoo.com>
2015-10-04 21:08:26 -0700

    Initial Commit

---
+++ Portfolio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Portfolio
+//
+//  Created by Jatin Pandey on 10/4/15.
+//  Copyright © 2015 Jatin Pandey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Portfolio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Portfolio
+//
+//  Created by Jatin Pandey on 10/4/15.
+//  Copyright © 2015 Jatin Pandey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
